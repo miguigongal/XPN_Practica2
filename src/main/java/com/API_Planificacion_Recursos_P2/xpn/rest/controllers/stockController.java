cff08eef1d5617a76a83147e103399e9c40cbc39
QuartzoneMusic <ivan.varela.gonzalez@udc.es>
2024-12-08 13:29:02 +0100

    Documentada la API con swagger

--- src/main/java/com/API_Planificacion_Recursos_P2/xpn/rest/controllers/stockController.java
+++ src/main/java/com/API_Planificacion_Recursos_P2/xpn/rest/controllers/stockController.java
@@ -14,17 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.API_Planificacion_Recursos_P2.xpn.model.common.exceptions.NegativeStockException;
 import com.API_Planificacion_Recursos_P2.xpn.model.entities.almacen;
 import com.API_Planificacion_Recursos_P2.xpn.model.services.stockService;
-import com.API_Planificacion_Recursos_P2.xpn.rest.dtos.almacenDto;
 import com.API_Planificacion_Recursos_P2.xpn.rest.dtos.updateParamsDto;
-import com.API_Planificacion_Recursos_P2.xpn.rest.conversors.almacenConversor;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api/stock")
+@Tag(name = "Stock Controller", description = "Controlador para gestionar el stock de los muebles en almacenes")
 public class stockController {
 
     @Autowired 
     stockService stockService;
 
+
+    @Operation(summary = "getCurrentStock", description = "Devuelve el stock actual de un mueble en un almacén específico.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Stock actual devuelto exitosamente"),
+        @ApiResponse(responseCode = "404", description = "No se encontró el mueble o el almacén")
+    })
     @GetMapping("/actual")
     public Integer getCurrentStock(@Validated @RequestParam Long muebleId, @RequestParam String almacenName) throws InstanceNotFoundException{
         int currentStock = stockService.getCurrentStock(muebleId, almacenName);
@@ -34,13 +44,24 @@ public class stockController {
         return currentStock;
     }
 
+
+    @Operation(summary = "getMaximumStock", description = "Devuelve el stock máximo de un mueble en un almacén")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Stock máximo devuelto exitosamente"),
+        @ApiResponse(responseCode = "404", description = "No se encontró el mueble o el almacén")
+    })
     @GetMapping("/max")
     public Integer getMaximumStock(@Validated @RequestParam Long muebleId, @RequestParam String almacenName) throws InstanceNotFoundException{
         int maximumStock = stockService.getMaxStock(muebleId, almacenName);
         System.out.println("max");
         return maximumStock;
     }
-  
+
+    @Operation(summary = "getMinimumStock", description = "Devuelve el stock mínimo de un mueble en un almacén")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Stock mínimo devuelto exitosamente"),
+        @ApiResponse(responseCode = "404", description = "No se encontró el mueble o el almacén")
+    })
     @GetMapping("/min")
     public Integer getMinimumStock(@Validated @RequestParam Long muebleId, @RequestParam String almacenName) throws InstanceNotFoundException{
         int minimumStock = stockService.getMinStock(muebleId, almacenName);
@@ -48,6 +69,12 @@ public class stockController {
         return minimumStock;
     }
 
+
+    @Operation(summary = "updateStock", description = "Actualiza el stock de un mueble en un almacén")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Stock actualizado exitosamente"),
+        @ApiResponse(responseCode = "400", description = "Stock negativo no permitido")
+    })
     @PostMapping("/updateStock")
     public void updateStock(@RequestBody updateParamsDto paramsDto) throws NegativeStockException{
         stockService.updateStock(paramsDto.getIdMueble(), paramsDto.getNombreAlmacen(), paramsDto.getActualStock());
@@ -57,14 +84,10 @@ public class stockController {
         
     }
 
-    /* 
-    @GetMapping("/recommend")
-    public almacenDto recommendAlmacen(){
-        almacen almacen = stockService.recommendAlmacen();
-        System.out.println("recomed");
-        return almacenConversor.toAlmacenDto(almacen);
-    }*/
-
+    @Operation(summary = "recommendAlmacen", description = "Recomienda un almacén adecuado para el mueble.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Recomendación de almacén exitosa")
+    })
     @GetMapping("/recommend")
     public String recommendAlmacen(){
         almacen almacen = stockService.recommendAlmacen();
