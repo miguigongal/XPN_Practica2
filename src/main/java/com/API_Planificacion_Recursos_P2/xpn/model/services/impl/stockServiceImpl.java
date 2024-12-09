a9c85cddbb81f0aa6d3b25bca9fed9e314c6ddff
miguigongal <miguigongal@gmail.com>
2024-12-09 23:17:24 +0100

    Eliminado comentarios

--- src/main/java/com/API_Planificacion_Recursos_P2/xpn/model/services/impl/stockServiceImpl.java
+++ src/main/java/com/API_Planificacion_Recursos_P2/xpn/model/services/impl/stockServiceImpl.java
@@ -60,17 +60,7 @@ public class stockServiceImpl implements stockService{
         stockDao.save(foundStock);
     }
 
-    /*
-    @Override
-    public almacen recommendAlmacen() {
-        List<almacen> allAlmacenes = almacenDao.findAll();
-        
-        Random random = new Random();
-        int randomIndex = random.nextInt(allAlmacenes.size());
-        almacen randomAlmacen = allAlmacenes.get(randomIndex);
 
-        return randomAlmacen;
-    }*/
     @Override
     public almacen recommendAlmacen() {
         List<almacen> allAlmacenes = almacenDao.findAll();
@@ -78,7 +68,6 @@ public class stockServiceImpl implements stockService{
         Random random = new Random();
         almacen randomAlmacen = null;
     
-        // Asegúrate de que el almacén con id = 1 no sea seleccionado
         while (randomAlmacen == null || randomAlmacen.getId() == 1) {
             int randomIndex = random.nextInt(allAlmacenes.size());
             randomAlmacen = allAlmacenes.get(randomIndex);
